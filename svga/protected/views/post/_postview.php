18a16e679c96daba9163113d459cf47ae325180a
xexu <jlexposito7@gmail.com>
2014-10-06 00:21:52 +0200

    arreglat fb comments

--- svga/protected/views/post/_postview.php
+++ svga/protected/views/post/_postview.php
@@ -40,7 +40,8 @@
 		<div class="fb-comments" data-href="<?= $data->url ?>" data-width="100%" data-numposts="5" data-colorscheme="light"></div>
 </div>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
